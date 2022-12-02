262c5a29fc1ad0c07d8f62430a4d6921abae9cf9
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-12-03 10:28:47 +1300

    testing auto commit.

--- test_commit.py
+++ test_commit.py
@@ -5,7 +5,7 @@ import git
 
 repo = git.Repo('/Users/jamie/Desktop/BASE/dev/automit/')
 
-# Provide a list of the files to stage
+# Provide a list of the file to stage
 repo.index.add(['test_commit.py'])
 # Provide a commit message
 repo.index.commit('testing auto commit.')
